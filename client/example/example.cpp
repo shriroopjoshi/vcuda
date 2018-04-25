6257b40c2534030340d4f91a74c990a82d339185
Shriroop <shriroop.joshi@gmail.com>
2018-04-25 10:58:36 -0500

    [NOT EXECUTABLE] Created an example
    * Created example directory which will contain all examples.
    * Created Makefile for example
    * Working on sending kernel through sockets

---
+++ client/example/example.cpp
@@ -0,0 +1,17 @@
+#include "vcuda.h"
+#include <iostream>
+
+int main() {
+    vcuda_client client ("localhost", 9000);
+    label_t c1 = client.vcudaMalloc(3, VC_INT);
+    std :: cout << "c = " << c1 << std :: endl;
+    label_t c2 = client.vcudaMalloc(5, VC_FLOAT);
+    std :: cout << "c = " << c2 << std :: endl;    
+    int arr[] = {1, 2, 3};
+    client.vcudaMemcpy(c1, arr, 3, vcudaMemcpyDeviceToHost);
+    float ptr[] = {2.5, 7.9, 6.3, 9.45, 8.2385};
+    client.vcudaMemcpy(c2, ptr, 5, vcudaMemcpyDeviceToHost);
+    label_t k1 = client.add_kernel("example.kr");
+    client.execute_kernel(k1);
+    return 0;
+}
